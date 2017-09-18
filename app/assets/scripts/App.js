216f30582da29696e3d53f3f3406efb876884217
Ram <ram.lamichhane@aol.com>
2017-09-18 13:54:14 -0500

    Added Modal to link to a button.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
 var mobileMenu = new MobileMenu(); 
@@ -11,3 +12,4 @@ new RevealOnScroll($(".testimonial"), "60%");
 
 
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
