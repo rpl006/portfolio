0abaa52c07f03af27d9d380eb4ab2774a47cf3b4
Ram <ram.lamichhane@aol.com>
2017-09-18 15:08:24 -0500

    Completd lazyloading branch

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11268,6 +11268,7 @@ var StickyHeader = function () {
 	function StickyHeader() {
 		_classCallCheck(this, StickyHeader);
 
+		this.lazyImages = (0, _jquery2.default)(".lazyload");
 		this.siteHeader = (0, _jquery2.default)(".site-header");
 		this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
 		this.createHeaderWaypoint();
@@ -11275,9 +11276,17 @@ var StickyHeader = function () {
 		this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 		this.createPageSectionWaypoints();
 		this.addSmoothScrolling();
+		this.refreshWayPoints();
 	}
 
 	_createClass(StickyHeader, [{
+		key: 'refreshWayPoints',
+		value: function refreshWayPoints() {
+			this.lazyImages.on('load', function () {
+				Waypoint.refreshAll();
+			});
+		}
+	}, {
 		key: 'addSmoothScrolling',
 		value: function addSmoothScrolling() {
 			this.headerLinks.smoothScroll();
