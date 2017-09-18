1ee06c82ca205ac9e8882704e1a4c4137bcea6c4
Ram <ram.lamichhane@aol.com>
2017-09-18 10:32:19 -0500

    waypoints added and .js edited accordingly

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 
 var mobileMenu = new MobileMenu(); 
+var revealOnScroll = new RevealOnScroll();
